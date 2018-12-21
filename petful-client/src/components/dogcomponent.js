b3bcd358c2ffa756558a83459e86b487ca57ccb6
mkcnly <mkcnly@gmail.com>
2018-12-21 14:13:56 -0800

    added dog props

--- petful-client/src/components/dogcomponent.js
+++ petful-client/src/components/dogcomponent.js
@@ -12,10 +12,28 @@ render(){
     return <p>loading...</p>
   }
   const dogName = this.props.dog.name;
+  const dogImage = this.props.dog.imageURL;
+  const imageDescription = this.props.dog.imageDescription;
+  const dogSex = this.props.dog.sex;
+  const dogAge = this.props.dog.age;
+  const dogBreed = this.props.dog.breed;
+  const dogStory = this.props.dog.story;
   return(
+    <span>
+  <img src={dogImage} alt={imageDescription}/>
+  <p>{dogStory}</p>
+  <dl>
+  <dt>Name:</dt>
+  <dd>{dogName}</dd>
+  <dt>Gender:</dt>
+  <dd>{dogSex}</dd>
+  <dt>Age:</dt>
+  <dd>{dogAge}</dd>
+  <dt>Breed:</dt>
+  <dd>{dogBreed}</dd>
+</dl>
   
-  <span>{dogName}</span>
-
+</span>
   )
 }
 
